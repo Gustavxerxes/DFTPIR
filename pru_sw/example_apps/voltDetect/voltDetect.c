db9dfc605fa52eed148470843446932d99ae4140
root <root@beaglebone.(none)>
2014-05-13 19:51:19 +0000

    fixat

--- pru_sw/example_apps/voltDetect/voltDetect.c
+++ pru_sw/example_apps/voltDetect/voltDetect.c
@@ -43,31 +43,39 @@ int main (void){
 				temp = matrix[i][k];
 				
 				//printf(" %d " , matrix[i][k]);	
-				if(temp < 260 || temp > 300){	
-				detection[i] = (6*detection[i] + 2*abs(matrix[i][k]-275))/8;
+				if(temp < 260 || temp > 290){	
+				detection[i] = (29*detection[i] + 3*abs(matrix[i][k]-275))/32;
 				//printf("\n chan %d : %04.2f \n", i , matrix[i][k]*3.2227);
-				}
-				detection[i] = (31*detection[i])/32;
-					
+				}else{
+				detection[i] = (62*detection[i])/64;
+				} 	
 				//printf(" detect %d : %4.2d ", i , detection[i]);
 				
 				
-				if((detection[i] > 100) && ((detection[(i+1)%4] > 30) || (detection[(i-1)%4] > 30)) && !alarm){
-					alarm = 50;
+				if((detection[i] > 100) && ((detection[(i+1)%4] > 40) || (detection[(i-1)%4] > 40))){
+					if(alarm<1){
+						alarm = 20;
+						system("echo high > /sys/class/gpio/gpio60/direction");
+					}
 					printf( "\n  ---- detection in chanels: %d, %d \n", i, (i+1)%4);
-					system("echo high > /sys/class/gpio/gpio60/direction");
 					
 					
 				}
 				
 			}
-			
+			/*
+	printf(" detect %d : %4.2d \n", 1 , detection[0]);
+	printf(" detect %d : %4.2d \n", 2 , detection[1]);
+	printf(" detect %d : %4.2d \n", 3 , detection[2]);
+	printf(" detect %d : %4.2d \n ", 4 , detection[3]);
+	
+	printf(" value: %d \n" , matrix[3][40]);
+	
+	printf("] \n");
+	*/		
 		}
 		
 	}
-	printf(" detect %d : %4.2d ", 4 , detection[4]);
-	printf(" %d " , matrix[4][40]);
-	printf("] \n");
 	
 
 	int prussStop(void); //good to have in end
